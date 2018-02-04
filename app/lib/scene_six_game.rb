6b2711a7ac00b86e0993b0bb21031d3aeb6cac7f
Amir Rajan <ar@amirrajan.net>
2018-02-04 12:04:52 -0600

    Space Invaders enemies are being removed if hit by bullet in the headless engine.

--- app/lib/scene_six_game.rb
+++ app/lib/scene_six_game.rb
@@ -1,36 +1,40 @@
 class SceneSixGame
+  ENEMY_SIZE = 50
   attr_accessor :ship_x,
                 :ship_y,
                 :target_x,
                 :target_y,
                 :bullet_location_y,
                 :bullet_location_x,
-                :enemy_x,
-                :enemy_y
+                :enemies
 
   def initialize
     @ship_x = 0
     @ship_y = 0
     @target_x = 0
     @target_y = 0
-    @enemy_x = 0
-    @enemy_y = 600
+    @enemies = 10.times.map do |i|
+      {
+        id: i.to_s,
+        x: (5 * -ENEMY_SIZE) + (i * ENEMY_SIZE),
+        y: 600
+      }
+    end
   end
 
   def update
     delta_x = (@target_x - @ship_x)
     @ship_x += delta_x * 0.2
 
-    @bullet_location_y += 1 if @bullet_location_y
+    @bullet_location_y += 10 if @bullet_location_y
 
     if @bullet_location_y && @bullet_location_y > 630
       @bullet_location_y = nil
     end
   end
 
-  def enemy_was_hit
-    @enemy_x = nil
-    @enemy_y = nil
+  def enemy_was_hit id
+    @enemies.delete(@enemies.find { |e| e[:id] == id })
     @bullet_location_y = nil
   end
 
