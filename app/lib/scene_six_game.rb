ead160710fa4af279c44aff05a8f89ea1a6641da
Amir Rajan <ar@amirrajan.net>
2018-02-04 11:14:56 -0600

    Headless space invaders engine can now fire a bullet.

--- app/lib/scene_six_game.rb
+++ app/lib/scene_six_game.rb
@@ -1,5 +1,10 @@
 class SceneSixGame
-  attr_accessor :ship_x, :ship_y, :target_x, :target_y
+  attr_accessor :ship_x,
+                :ship_y,
+                :target_x,
+                :target_y,
+                :bullet_location_y,
+                :bullet_location_x
 
   def initialize
     @ship_x = 0
@@ -11,5 +16,12 @@ class SceneSixGame
   def update
     delta_x = (@target_x - @ship_x)
     @ship_x += delta_x * 0.2
+
+    @bullet_location_y += 1 if @bullet_location_y
+  end
+
+  def fire_bullet
+    @bullet_location_x = @ship_x
+    @bullet_location_y = @ship_y
   end
 end
